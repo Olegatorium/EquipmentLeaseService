aa06c1f26f11ca01deeedfecc3a8482c65e0eb56
Oleh <olegatorium@gmail.com>
2025-01-22 09:51:00 +0100

    Enabled Swagger UI in production environment

--- EquipmentLeaseService.API/Program.cs
+++ EquipmentLeaseService.API/Program.cs
@@ -72,12 +72,9 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseMiddleware<ApiKeyMiddleware>();
 app.UseMiddleware<ExceptionHandlerMiddleware>();
